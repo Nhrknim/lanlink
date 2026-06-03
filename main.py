98bee2d69b15d5fda2e66054bdd0ed00ecc67d86
Niharika S <niharikanair777@gmail.com>
2026-06-03 19:20:21 +0530

    Restructure application files

---
+++ main.py
@@ -0,0 +1,12 @@
+import sys
+
+from PyQt6.QtWidgets import QApplication
+from ui.login import LoginWindow
+
+
+app = QApplication(sys.argv)
+
+window = LoginWindow()
+window.show()
+
+sys.exit(app.exec())
--- ui/chat.py
+++ ui/chat.py
@@ -18,7 +18,6 @@ from PyQt6.QtWidgets import (
     QFileDialog
 )
 
-
 class ChatWindow(QWidget):
 
     message_received = pyqtSignal(dict)
@@ -362,65 +361,3 @@ class ChatWindow(QWidget):
         self.chat_title.setText(
             f"Chat with {user}"
         )
-
-class LoginWindow(QWidget):
-
-    def __init__(self):
-        super().__init__()
-
-        self.setWindowTitle("LANLink Login")
-        self.resize(300, 150)
-
-        layout = QVBoxLayout()
-
-        self.username_input = QLineEdit()
-        self.username_input.setPlaceholderText("Username")
-
-        self.ip_input = QLineEdit()
-        self.ip_input.setPlaceholderText("Server IP")
-        self.ip_input.setText("127.0.0.1")
-
-        self.connect_button = QPushButton("Connect")
-
-        layout.addWidget(self.username_input)
-        layout.addWidget(self.ip_input)
-        layout.addWidget(self.connect_button)
-
-        self.setLayout(layout)
-
-        self.username_input.returnPressed.connect(
-            self.connect_to_chat
-        )
-        self.ip_input.returnPressed.connect(
-            self.connect_to_chat
-        )
-
-        self.connect_button.clicked.connect(
-            self.connect_to_chat
-        )
-
-    def connect_to_chat(self):
-
-        username = self.username_input.text().strip()
-        server_ip = self.ip_input.text().strip()
-
-        if not username:
-            return
-
-        self.chat_window = ChatWindow(
-            username,
-            server_ip
-        )
-
-        self.chat_window.show()
-
-        self.close()
-
-
-app = QApplication(sys.argv)
-
-window = LoginWindow()
-
-window.show()
-
-sys.exit(app.exec())
