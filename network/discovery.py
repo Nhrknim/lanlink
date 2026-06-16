dcacec9bf6ba6052062b8118a86c2e0845bc9325
Niharika S <niharikanair777@gmail.com>
2026-06-16 22:51:46 +0530

    Fix LAN room discovery scanning

--- network/discovery.py
+++ network/discovery.py
@@ -21,21 +21,37 @@ def broadcast_room(room_name, host_name):
         time.sleep(2)
 
 def find_rooms():
-
     udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    udp.setsockopt(
+        socket.SOL_SOCKET,
+        socket.SO_REUSEADDR,
+        1
+    )
+
     udp.bind(("", DISCOVERY_PORT))
+    udp.settimeout(0.5)
 
     rooms = {}
+    start_time = time.time()
 
-    while True:
+    while time.time() - start_time < 3:
+        try:
+            data, address = udp.recvfrom(1024)
 
-        data, address = udp.recvfrom(1024)
+            message = json.loads(data.decode())
 
-        message = json.loads(data.decode())
+            rooms[message["room"]] = {
+                "host": message["host"],
+                "ip": address[0]
+            }
 
-        rooms[message["room"]] = {
-            "host": message["host"],
-            "ip": address[0]
-        }
+        except socket.timeout:
+            continue
 
-        return rooms
+        except Exception as e:
+            print("Discovery error:", e)
+
+    udp.close()
+
+    return rooms
